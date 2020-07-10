29a4c786ac3f91a39d0972c6a44228c4193f4341
Corentin Pane <corentin.pane@eatops.com>
2020-07-10 22:03:28 +0200

    Updates

---
+++ precache-manifest.a83889de34e4a3c18e4eb50b375ac1a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0aa2a09a8238e01dff0f4dcc7251338",
+    "url": "/albertron/index.html"
+  },
+  {
+    "revision": "f82068de17fb5f74f126",
+    "url": "/albertron/static/css/main.8a1a4f14.chunk.css"
+  },
+  {
+    "revision": "8ba30eb4eaee81f316e3",
+    "url": "/albertron/static/js/2.e1697bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/albertron/static/js/2.e1697bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82068de17fb5f74f126",
+    "url": "/albertron/static/js/main.7f76a558.chunk.js"
+  },
+  {
+    "revision": "ee802883b3cd2e37b9d0",
+    "url": "/albertron/static/js/runtime-main.bc1dd70f.js"
+  }
+]);
